31b81391b0d198a02bc713ecb87527546afd0aef
Christian Maldonado <chris.maldonado99@gmail.com>
2020-06-03 12:02:06 -0700

    added react porftolio scripts

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ function Footer()  {
             <Container fluid={true}>
                 <Row className="border-top justify-content-between p-3"> 
                     <Col className="p-0" sd={12}>
-                        Christain Maldonado
+                        Christian Maldonado
                     </Col>
                     <Col className="p-0 d-flex justify-content-end" md={3}>
                         This site was made by Christian Maldonado
