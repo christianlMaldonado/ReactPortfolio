3b120c3b72edc2d01ba6311c4cdb2b6aa56482e5
Christian Maldonado <chris.maldonado99@gmail.com>
2020-06-03 12:04:35 -0700

    Updates

---
+++ precache-manifest.f4c6a497d901c350553f67edf048afee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ead78cb59707f4a7f717260c90aaa1",
+    "url": "/christianlMaldonado/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "2e2a1d9f80908a07b8e2",
+    "url": "/christianlMaldonado/ReactPortfolio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "f1939738f12e40bd8a9b",
+    "url": "/christianlMaldonado/ReactPortfolio/static/css/main.d79695cf.chunk.css"
+  },
+  {
+    "revision": "2e2a1d9f80908a07b8e2",
+    "url": "/christianlMaldonado/ReactPortfolio/static/js/2.5b81c283.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/christianlMaldonado/ReactPortfolio/static/js/2.5b81c283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1939738f12e40bd8a9b",
+    "url": "/christianlMaldonado/ReactPortfolio/static/js/main.74607b6c.chunk.js"
+  },
+  {
+    "revision": "df47513d805cca66605a",
+    "url": "/christianlMaldonado/ReactPortfolio/static/js/runtime-main.3b768554.js"
+  },
+  {
+    "revision": "9ccf6a27dce216b1f774214e7ebf16d1",
+    "url": "/christianlMaldonado/ReactPortfolio/static/media/image1.9ccf6a27.jpg"
+  },
+  {
+    "revision": "62d7d7dded871121278246cb4f479471",
+    "url": "/christianlMaldonado/ReactPortfolio/static/media/logo.62d7d7dd.jpg"
+  }
+]);
