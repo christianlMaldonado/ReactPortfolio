a7516ac18923f606e3f957a7bc2b33a8cef3186a
Christian Maldonado <41269975+christianlMaldonado@users.noreply.github.com>
2021-01-07 13:32:48 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: "Christian Maldonado Developr Portfolio",
+      title: "Christian Maldonado Developer Portfolio",
       headerLinks: [
         { title: "Home", path: "/" },
         { title: "About", path: "/about" },
